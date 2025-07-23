75554d7e528dd5ce3605d41f98fa4523047c7567
José Rodríguez <58joserc@gmail.com>
2025-07-23 12:39:10 +0200

    Cuadrícula inicial

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById(caja)
+
+let numfilas = 10
+let numcolumnas1 = 10
+
+for (numfilas;numfilas>=0;numfilas--) {
+    let numcolumnas2 = numcolumnas1 
+    let fila = document.createElement("div")
+    fila.classList.add("fila")
+    caja.appendChild(fila)
+    for (numcolumnas2;numcolumnas2>=0;numcolumnas2--) {
+        let celda = document.createElement("div")
+        celda.classList.add("celda")
+        fila.appendChild(celda)
+    }
+}
