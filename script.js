918af29dc65a3d5b41e5d0639f42e93cf011d4b1
José Rodríguez <58joserc@gmail.com>
2025-07-25 12:41:20 +0200

    Selector de dificultad

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
-let select=document.getElementById(dificultad)
+let select=document.getElementById("dificultad")
+let dificultad;
+let numfilas, numcolumnas1;
 
-console.log(select)
+function CrearTablero () {
+    document.getElementById(caja)
+    caja.innerHTML = "";
 
-let numfilas = 10
-let numcolumnas1 = 10
+    for (numfilas;numfilas>=0;numfilas--) {
+        let numcolumnas2 = numcolumnas1;
+        let fila = document.createElement("div");
+        fila.classList.add("fila");
+        caja.appendChild(fila);
+        for (numcolumnas2;numcolumnas2>=0;numcolumnas2--) {
+            let celda = document.createElement("div");
+            celda.classList.add("celda");
+            fila.appendChild(celda);
+        };
+    };
+};
 
-document.getElementById(caja)
-
-for (numfilas;numfilas>=0;numfilas--) {
-    let numcolumnas2 = numcolumnas1 
-    let fila = document.createElement("div")
-    fila.classList.add("fila")
-    caja.appendChild(fila)
-    for (numcolumnas2;numcolumnas2>=0;numcolumnas2--) {
-        let celda = document.createElement("div")
-        celda.classList.add("celda")
-        fila.appendChild(celda)
-    }
-}
+select.addEventListener('change', () => {
+    dificultad = select.value;
+    if (dificultad == "facil"){
+        numfilas = 9
+        numcolumnas1 = 9
+    } else if (dificultad == "medio") {
+        numfilas = 16
+        numcolumnas1 = 16
+    } else if (dificultad == "dificil") {
+        numfilas = 16
+        numcolumnas1 = 30
+    };
+    console.log(dificultad);
+    CrearTablero();
+})
