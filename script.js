299dbaaeb54bf353e13684c6cf833f2cf1831378
José Rodríguez <58joserc@gmail.com>
2025-07-28 09:33:00 +0200

    Avance en colocación de minas

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
 let select=document.getElementById("dificultad")
 let dificultad;
-let numfilas, numcolumnas1;
+let numfilas, numcolumnas1, idcelda, numminas, sitiomina;
 
 function CrearTablero () {
     document.getElementById(caja)
     caja.innerHTML = "";
+    idcelda = 1;
 
-    for (numfilas;numfilas>=0;numfilas--) {
+    for (numfilas;numfilas>=1;numfilas--) {
         let numcolumnas2 = numcolumnas1;
         let fila = document.createElement("div");
         fila.classList.add("fila");
         caja.appendChild(fila);
-        for (numcolumnas2;numcolumnas2>=0;numcolumnas2--) {
+        for (numcolumnas2;numcolumnas2>=1;numcolumnas2--) {
             let celda = document.createElement("div");
             celda.classList.add("celda");
+            celda.setAttribute("id", `celda${idcelda}`);
             fila.appendChild(celda);
+            idcelda++
         };
     };
 };
+
+function AñadirMinas () {
+    let maxtablero = (numfilas*numcolumnas1)
+    sitiomina = Math.floor(Math.random() * maxtablero) + 1;
+    for (numminas;numminas>=0;numminas--) {
+
+    }
+}
 
 select.addEventListener('change', () => {
     dificultad = select.value;
     if (dificultad == "facil"){
         numfilas = 9
         numcolumnas1 = 9
+        numminas = 10
     } else if (dificultad == "medio") {
         numfilas = 16
         numcolumnas1 = 16
+        numminas = 40
     } else if (dificultad == "dificil") {
         numfilas = 16
         numcolumnas1 = 30
+        numminas = 99
     };
-    console.log(dificultad);
     CrearTablero();
 })
